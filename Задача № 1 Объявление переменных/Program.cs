92d227f214e445400fdaa9b189e319ad37d0e7e7
Avolyx <Denis.O.Davydov@yandex.ru>
2023-06-05 01:46:42 +0800

    Добавьте файлы проекта.

---
+++ Задача № 1 Объявление переменных/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Задача_1_Объявление_переменных
+{
+    //Попрактикуйтесь в создании переменных, объявить 10 переменных разных типов.
+    //Напоминание: переменные именуются с маленькой буквы, если название состоит из нескольких слов, то комбинируем их следующим образом - названиеПеременной.
+    //Также имя всегда должно отражать суть того, что хранит переменная.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte health;
+            float medicinesPrice;
+            uint playerHealth;
+            string userNames;
+            ushort money;
+            bool willBeHit;
+            short courseUSD;
+            char designation;
+            long numberPi;
+            double programNumber;
+        }
+    }
+}
